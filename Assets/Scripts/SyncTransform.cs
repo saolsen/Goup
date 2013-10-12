2c4e95cfd6258f873c6ceef7c149381e8dda0c38
Stephen Olsen <steve.olsen@gmail.com>
2013-10-12 15:38:41 -0400

    deleted stuff, starting over

--- Assets/Scripts/SyncTransform.cs
+++ Assets/Scripts/SyncTransform.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class SyncTransform : MonoBehaviour {
-	
+
 	public Transform child = null;
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (child != null) {
